35ba76a2cc5f8f415b391d53ec5eaa71a8d919f4
Andreas Rasmussen <andreasrefras@gmail.com>
2016-10-03 20:10:13 +0200

    Initialized Playground-route - proof of horizontal scroll behind logo concept

--- src/routes/childRoutes/type16/indexRoute/components/Container/index.js
+++ src/routes/childRoutes/type16/indexRoute/components/Container/index.js
@@ -1,75 +1,42 @@
 /* ----- TYPE16 ----- */
 
 // Styles
-
 import './index.scss';
 
 // Lib
-
 import React, { Component } from 'react';
 
 // Components
-
-import Button from 'components/Button';
 import Hero from 'components/Hero';
-import GridBlock from 'components/GridBlock';
 
 // Images
+import logo from './assets/logo-type16-white.svg';
+import watercolorTest from './assets/vandfarve-test.gif';
 
 // Class
-
 class Container extends Component {
 
-  constructor(props) {
+  constructor(props) { super(props); }
 
-    super(props);
-
-  }
+  componentWillMount() { document.body.style.overflowY = 'hidden'; }
+  componentWillUnmount() { document.body.style.overflowY = 'auto'; }
 
   render() {
-
     return (
-      <div className="pageAbout">
-
+      <div className="page-type16">
         <Hero>
-          <h1 className="fontSize-display fontFamily-logo textTransform-uppercase">TYPE16</h1>
+          <img className="logo" src={ logo } height="56" />
         </Hero>
-
-        <GridBlock columns="3">
-          <div>
-            <Button linkTo="/">Projects</Button>
-          </div>
-          <div>
-            <Button linkTo="/" link>Testlink</Button>
-          </div>
-          <div>
-            <ul>
-              <li>Playful</li>
-              <li>Calm</li>
-              <li>One thing at a time</li>
-              <li>Devils in the details</li>
-              <li>Etc.</li>
-            </ul>
-          </div>
-        </GridBlock>
-
+        <img src={ watercolorTest } />
       </div>
     );
-
   }
-
 }
 
 // PropTypes
-
 Container.propTypes = {
 
 };
 
-Container.defaultProps = {
-
-};
-
 // Export
-
 export default Container;
